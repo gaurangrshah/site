29662988fa79fdc4c637a73081551b2a9fcc76b1
gaurangrshah <gaurang.r.shah@gmail.com>
2021-05-01 19:02:01 -0400

    update twitter card

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,36 @@ import Head from "next/head";
 import { Box, Flex } from "@chakra-ui/react";
 import { Splash } from "@/components/structure/splash";
 
-
 export default function Landing() {
   return (
     <>
       <Head>
-        <title>G. Shah Dev</title>
-        <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='description'
+          content='Full Stack Developer - Learning while building in public'
+        />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://gshahdev.com/' />
+        <meta property='og:title' content='G. Shah Dev' />
+        <meta
+          property='og:description'
+          content='Full Stack Developer - Learning while building in public'
+        />
         <meta
           property='og:image'
+          content='https://cdn.jsdelivr.net/gh/gaurangrshah/_shots@master/scrnshots/image.png'
+        />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://gshahdev.com/' />
+        <meta property='twitter:title' content='G. Shah Dev' />
+        <meta
+          property='twitter:description'
+          content='Full Stack Developer - Learning while building in public'
+        />
+        <meta
+          property='twitter:image'
           content='https://cdn.jsdelivr.net/gh/gaurangrshah/_shots@master/scrnshots/image.png'
         />
       </Head>
