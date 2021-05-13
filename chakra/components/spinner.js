d767e03ba4d0225ab8bf62c6fad64fe97b06e0db
gaurangrshah <gaurang.r.shah@gmail.com>
2021-05-13 16:11:23 -0400

    fix: test deploy - spotify

--- chakra/components/spinner.js
+++ chakra/components/spinner.js
@@ -2,8 +2,8 @@ import { Box, Flex, Spinner as ChSpinner } from "@chakra-ui/react";
 
 export function Spinner({ ...rest }) {
   return (
-    <Flex display='flex' minH='100%' flex={1}>
-      <ChSpinner m='auto' zIndex='popover' {...rest} />
-    </Flex>
+    // <Flex display='flex' minH='100%' flex={1}>
+    <ChSpinner m='auto' zIndex='popover' {...rest} />
+    // </Flex>
   );
 }
