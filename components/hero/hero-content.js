ab5c06c73395be862f1af7154853d74bdaada8b3
gaurangrshah <gaurang.r.shah@gmail.com>
2021-05-21 14:51:09 -0400

    removes hero blurbox

--- components/hero/hero-content.js
+++ components/hero/hero-content.js
@@ -51,13 +51,13 @@ export const HeroContent = ({ children, ...rest }) => {
           Hi-Ya! I'm Gaurang!
         </Box>
       </Heading>
-      <Box
+      {/* <Box
         // maxW='350px'
         h='9px'
         filter='blur(1.4rem) opacity(0.3)'
         bg='brand.900'
         mt={-20}
-      />
+      /> */}
       <Box pt={3} pl={2}>
         <Text
           fontSize='md'
