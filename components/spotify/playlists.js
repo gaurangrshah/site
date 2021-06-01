063c3704fd18616edf8cc98d678758b65ec128a9
gaurangrshah <gaurang.r.shah@gmail.com>
2021-05-31 20:58:44 -0400

    sandbox refactor/cleanup

--- components/spotify/playlists.js
+++ components/spotify/playlists.js
@@ -1,18 +1,22 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import {
   Box,
   Button,
   Flex,
-  Image,
+  Image as ChImage,
   Link,
   SimpleGrid,
+  Skeleton,
   Text,
 } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
 
-import { ShowJson } from "@/chakra/components/show-json";
+import { MotionBox } from "../framer/motion-box";
 
 export const Playlists = () => {
   const [data, setData] = useState(null);
+  const [selected, setSelected] = useState(null);
 
   useEffect(async () => {
     if (data) return;
@@ -20,15 +24,21 @@ export const Playlists = () => {
       `${process.env.NEXT_PUBLIC_API_URL}/spotify/my-playlists`
     );
     const d = await response.json();
-    console.log(d.response);
+    // console.log(d.response);
     const total = d?.response?.total;
     setData(d.data);
   }, []);
 
+  const handlePlaylistHover = (e) => {
+    e.preventDefault();
+    console.log("test", e.target.getAttribute("data-index"));
+    setSelected(e.target.getAttribute("data-index"));
+  };
+
   return (
     <Box
       position='relative'
-      bg='brand.dark'
+      bg='rgba(226, 250, 219, 0.25)'
       opacity='0.9'
       w={["70%", "container.xs", "container.lg"]}
       boxShadow='md'
@@ -38,41 +48,58 @@ export const Playlists = () => {
       my='auto'
       mb={"20em"}
     >
-      <Flex pos='relative' w='full' flexWrap='wrap'>
+      <SimpleGrid pos='relative' w='full' columns={4} rowGap={20}>
         {data?.length &&
-          data?.map((pl) => {
+          data?.map((pl, i) => {
             console.log(pl);
             return (
-              <Box
-                pos='relative'
+              <MotionBox
                 key={pl?.id}
-                color='white'
-                maxW='200px'
-                mx='auto'
-                border='1px'
                 as={Link}
                 href={pl?.external_urls?.spotify}
                 isExternal
+                pos='relative'
+                color='white'
+                maxW='200px'
+                mx='auto'
+                boxShadow='md'
+                initial={{ transform: `scale(1)` }}
+                whileHover={{ transform: `scale(1.2)` }}
+                onHoverStart={handlePlaylistHover}
+                whileTap={{ transform: `scale(0.9)` }}
+                data-index={i}
               >
-                <Image
-                  src={pl?.images[1]?.url}
-                  alt={pl?.name}
-                  w='200px'
-                  h='200px'
-                />
-                <Text
-                  pos='absolute'
-                  left='50%'
-                  right='50%'
-                  transform='translate(-50%, -50%)'
-                  w='full'
-                >
-                  {pl?.name}
-                </Text>
-              </Box>
+                <Skeleton isLoaded={pl?.images[1]?.url} w='200px' h='200px'>
+                  <Image
+                    src={pl?.images[1]?.url}
+                    alt={pl?.name}
+                    layout='intrinsic'
+                    width='200px'
+                    height='200px'
+                  />
+                </Skeleton>
+                <AnimatePresence>
+                  {selected === i && (
+                    <MotionBox
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      position='relative'
+                      w='full'
+                      display='flex'
+                      justifyContent='center'
+                      alignItems='center'
+                      flexDirection='row'
+                      color='white'
+                      zIndex={1}
+                    >
+                      <Text w='full'>{pl?.name}</Text>
+                    </MotionBox>
+                  )}
+                </AnimatePresence>
+              </MotionBox>
             );
           })}
-      </Flex>
+      </SimpleGrid>
     </Box>
   );
 };
--- pages/feature-spotify.js
+++ pages/feature-spotify.js
@@ -1,17 +1,14 @@
 import Head from "next/head";
-import { Box, Flex, VStack } from "@chakra-ui/react";
+import { Box, SimpleGrid, VStack } from "@chakra-ui/react";
 import { Splash } from "@/components/structure/splash";
 import { Playlists } from "@/components/spotify/playlists";
 import { NowPlaying } from "@/components/spotify/now-playing";
-
 
 export default function Sandbox() {
   return (
     <VStack>
       <NowPlaying />
-      <Flex m='auto auto' overflow='auto' maxW='full'>
-        <Playlists />
-      </Flex>
+      <Playlists />
     </VStack>
   );
 }
