8638f66202bd9c208408270ad26a7411461add9e
gaurangrshah <gaurang.r.shah@gmail.com>
2021-06-01 03:15:03 -0400

    adds list

---
+++ components/list.js
@@ -0,0 +1,92 @@
+import {
+  Box,
+  chakra,
+  Flex,
+  Icon,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+import { PathIcon } from "@/components/icons/path-icon";
+import { otherIcons } from "./icons/other";
+
+export function List() {
+  return (
+    <Stack
+      spacing={{ base: 10, md: 0 }}
+      display={{ md: "grid" }}
+      gridTemplateColumns={{ md: "repeat(2,1fr)" }}
+      gridColumnGap={{ md: 8 }}
+      gridRowGap={{ md: 10 }}
+    >
+      <Feature
+        title='Jamstack aficionado'
+        icon={<PathIcon icon={otherIcons.jam} />}
+      >
+        I'm a huge fan of jamstack architecture. I think it's the pinnacle of
+        the separation of concerns ideaology.
+      </Feature>
+
+      <Feature
+        title='Get  $h!t done'
+        icon={<PathIcon icon={otherIcons.bleep} />}
+      >
+        I'm an introvert masquerading as an extrovert in order to get $h!t done.
+      </Feature>
+
+      <Feature
+        title='Fail to succeed'
+        icon={<PathIcon icon={otherIcons.fail} />}
+      >
+        I've failed at more things than most people attempt, but for some reason
+        that only motivates me more.
+      </Feature>
+      <Feature title='Oontz oontz' icon={<PathIcon icon={otherIcons.record} />}>
+        I'm a die-hard music lover, I enjoy just about anything with a good
+        groove to it, but I tend to lean towards deep melodic soulful house and
+        techno.
+      </Feature>
+    </Stack>
+  );
+}
+
+const Feature = (props) => {
+  return (
+    <Flex bg='brand.300' p={3} borderRadius='5px'>
+      <Flex shrink={0}>
+        <Flex
+          alignItems='center'
+          justifyContent='center'
+          h={16}
+          w={16}
+          rounded='md'
+          bg={useColorModeValue("brand.500")}
+          color='white'
+        >
+          <Icon
+            boxSize={9}
+            fill='currentColor'
+            viewBox='0 0 24 24'
+            // stroke='currentColor'
+            aria-hidden='true'
+          >
+            {props.icon}
+          </Icon>
+        </Flex>
+      </Flex>
+      <Box ml={4}>
+        <chakra.dt
+          fontSize='lg'
+          fontWeight='medium'
+          lineHeight='6'
+          color={useColorModeValue("gray.900")}
+        >
+          {props.title}
+        </chakra.dt>
+        <chakra.dd mt={2} color={useColorModeValue("gray.500", "gray.400")}>
+          {props.children}
+        </chakra.dd>
+      </Box>
+    </Flex>
+  );
+};
---
+++ components/structure/_header-content.js
@@ -0,0 +1,190 @@
+// @SCOPE:  used to render header content into app scaffold
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+import {
+  chakra,
+  Box,
+  Button,
+  CloseButton,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  useDisclosure,
+  VisuallyHidden,
+  VStack,
+  IconButton,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { HamburgerIcon, PhoneIcon } from "@chakra-ui/icons";
+
+import { LogoIcon } from "@/components/logo-icon";
+import { ChNextButtonLink } from "../next-link";
+
+import { is } from "@/utils/validator";
+import { Drawer } from "@/chakra/components/drawer";
+import { useColor } from "@/chakra/hooks/use-color";
+
+const routes = {
+  home: "/",
+  about: "/about",
+  services: "/services",
+  contact: "/contact",
+  faqs: "/faqs",
+};
+
+export const FullLogo = ({
+  src = "/logo-blue.png",
+  imageProps = { width: "182px", height: "64px" },
+  headingAs = "h1",
+}) => {
+  return (
+    <Flex justify='space-between' align='center' px={{ base: 6, lg: 0 }}>
+      <chakra.a
+        href='/'
+        title='falcon home page'
+        display='flex'
+        alignItems='center'
+      >
+        <Box w='40px' h='40px'>
+          <LogoIcon />
+        </Box>
+        <VisuallyHidden>
+          <Heading as={headingAs} fontSize='4xl' fontWeight='bold'>
+            GSDev
+          </Heading>
+          <chakra.p>Driving School</chakra.p>
+        </VisuallyHidden>
+      </chakra.a>
+    </Flex>
+  );
+};
+
+const NavLinks = ({ onClose, ...rest }) => {
+  return (
+    <Flex
+      flexDirection={{ base: "column", md: "row" }}
+      alignItems={{ base: "flex-end", md: "initial" }}
+    >
+      {!is.empty(routes) &&
+        Object.keys(routes).map((key, i) => (
+          <ChNextButtonLink
+            key={key}
+            href={routes[key]}
+            isActive={routes[i] === routes[key]}
+            onClick={onClose}
+            _active={{
+              textDecoration: "underline",
+              color: "red",
+            }}
+            {...rest}
+          >
+            {key}
+          </ChNextButtonLink>
+        ))}
+    </Flex>
+  );
+};
+
+export function CallLink({ ...rest }) {
+  return (
+    <>
+      <ChNextButtonLink
+        href='tel: +12018655225'
+        color='blue.800'
+        variant='link'
+        {...rest}
+      >
+        <VStack>{rest.children}</VStack>
+      </ChNextButtonLink>
+    </>
+  );
+}
+
+export function HeaderContent({ bg }) {
+  const mobileNav = useDisclosure();
+  const { color } = useColor();
+
+  return (
+    <>
+      <Box w='full'>
+        <Flex
+          alignItems='center'
+          justifyContent='space-between'
+          mx='auto'
+          px={{ base: 2, sm: 4 }}
+          py={4}
+        >
+          <FullLogo />
+          <HStack display='flex' alignItems='center' spacing={1}>
+            <HStack
+              justify='flex-end'
+              spacing={1}
+              mr={1}
+              // color='gray.700'
+              display={{ base: "none", md: "inline-flex" }}
+            >
+              <NavLinks
+                chProps={{
+                  color: color("navLink"),
+                  variant: "link",
+                  textTransform: "capitalize",
+                  pr: 6,
+                }}
+              />
+            </HStack>
+            <CallLink />
+            <Box display={{ base: "inline-flex", md: "none" }}>
+              <IconButton
+                display={{ base: "flex", md: "none" }}
+                aria-label='Open menu'
+                fontSize='20px'
+                color='blue.800'
+                variant='ghost'
+                icon={<HamburgerIcon />}
+                onClick={mobileNav.onOpen}
+              />
+            </Box>
+          </HStack>
+        </Flex>
+      </Box>
+      <Drawer
+        content={{
+          header: {
+            Component: FullLogo,
+            props: {
+              headingAs: "h2",
+            },
+          },
+          body: {
+            Component: NavLinks,
+            props: {
+              onClose: mobileNav.onClose,
+              chProps: {
+                my: { base: "4", lg: 0 },
+                fontSize: "2xl",
+                textTransform: "uppercase",
+                py: 3,
+                mr: { base: 12, lg: 0 },
+                variant: "link",
+              },
+            },
+          },
+          footer: {
+            Component: CallLink,
+            props: {
+              fontSize: "2xl",
+              float: "right",
+              color: "#FFF",
+              bg: "#FAD201",
+              w: "full",
+              py: 12,
+            },
+          },
+        }}
+        {...mobileNav}
+      />
+    </>
+  );
+}
