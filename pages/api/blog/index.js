90476660470a8ef6c2af8541691adc271544c163
gaurangrshah <gaurang.r.shah@gmail.com>
2021-06-11 10:00:03 -0400

    test build issue

--- pages/api/blog/index.js
+++ pages/api/blog/index.js
@@ -4,8 +4,9 @@ export default async function handler(req, res) {
   // @SCOPE:  get published articles from dev.to my username
 
   const response = await devApi.getArticles({ username: "gsdev", page: 1 });
+  console.log("🚀 ~ file: index.js ~ line 7 ~ handler ~ response", response);
 
-  if(response?.length) return res.json(response);
+  if (response?.length) return res.json(response);
 
-  return res.json({error: {message: "An unknown error has occured."}})
+  return res.json({ error: { message: "An unknown error has occured." } });
 }
