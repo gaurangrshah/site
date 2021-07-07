e500067c2b011440f317bcb75de4696affcd8b69
gaurangrshah <gaurang.r.shah@gmail.com>
2021-07-06 21:18:44 -0400

    cleanup

--- pages/api/blog/[slug].js
+++
@@ -1,6 +0,0 @@
-import { devApi } from "@/lib/dev-to";
-
-export default async function handler(req, res) {
-  // @SCOPE:  set authToken cookie to request object
-  return res.json({ post: await devApi.getArticleBySlug(req.query.slug) });
-}
